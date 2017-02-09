adc3d9d2c8ab6f9db5b4a1c250c98d6d4280f606
AJUdensi <a.joshuaudensi@gmail.com>
2017-02-09 10:52:27 +0100

    error in line breaks fixed

--- findMinMax/library.js
+++ findMinMax/library.js
@@ -2,38 +2,28 @@
 
 module.exports = {
 
-  findMinMax: function (arr) {
-
-    if (arguments > 1) 
-      return 'Error';
+  findMinMax: function(arr) {
+    
+    if(arguments > 1) return 'Error';
     var max = arr[0];
     var min = arr[0];
     /** Throw TypeError if argument is not an array */
-    if (!(arr.constructor === Array)) 
-      throw new TypeError('Invalid arguments: Only an array is required');
-    
+    if(!(arr.constructor === Array)) throw new TypeError('Invalid arguments: Only an array is required');
+
     for (var index in arr) {
       /** Throw TypeError if an array value is not a number */
-      if (!(parseFloat(arr[index]))) 
-        throw new TypeError('Invalid array: Only numbers should be in the array');
-      
+      if(!(parseFloat(arr[index]))) throw new TypeError('Invalid array: Only numbers should be in the array');
       /** Checks and assign minimum and maximum values to man, mix respectively
        * in every iteration
       */
-      max = ((max < arr[index])
-        ? arr[index]
-        : max);
-      min = ((min > arr[index])
-        ? arr[index]
-        : min);
+      max = ((max < arr[index]) ? arr[index] :  max);
+      min = ((min > arr[index]) ? arr[index] : min);
 
-    }
-    /** Return result
+    } 
+    /** Return result 
      * if min is equal to max return any single of them
      * else return min and max as integers in an array
     */
-    return (min === max)
-      ? [parseInt(min)] || [parseInt(max)]
-      : [parseInt(min), parseInt(max)];
+    return (min ===  max) ? [parseInt(min)] || [parseInt(max)]: [parseInt(min), parseInt(max)];
   }
 }
