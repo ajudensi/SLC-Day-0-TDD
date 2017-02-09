0b99df62b01f9675feaecb40dfbb4b6bf09143ef
AJUdensi <a.joshuaudensi@gmail.com>
2017-02-09 10:28:18 +0100

    11 test cases for fizzBuzz passed

--- tests/fizzBuzz.js
+++ tests/fizzBuzz.js
@@ -5,6 +5,16 @@ var lib = require('../fizzBuzz/library.js');
 
 describe("Fizz Buzz tests ", function() {
 
+  it("should determine the fizzBuzz function is defined", function() {
+    expect(lib.fizzBuzz(0)).toBeDefined();
+  });
+
+  it("should throw an error if argument is not a number", function(){
+    expect(function(){lib.fizzBuzz({});}).toThrowError('Only a numbe is required');
+    expect(function(){lib.fizzBuzz('abc');}).toThrowError('Only a numbe is required');
+    expect(function(){lib.fizzBuzz([]);}).toThrowError('Only a numbe is required');
+  })
+
   it("should return `Fizz` for number divisible by 3", function() {
     expect(lib.fizzBuzz(3)).toEqual('Fizz');
   });
@@ -17,14 +27,6 @@ describe("Fizz Buzz tests ", function() {
     expect(lib.fizzBuzz(15)).toEqual('FizzBuzz');
   });
 
-  it("should return `fizzBuzz` for 45", function() {
-    expect(lib.fizzBuzz(45)).toEqual('FizzBuzz');
-  });
-
-  it("should return `fizzBuzz` for 90", function() {
-    expect(lib.fizzBuzz(90)).toEqual('FizzBuzz');
-  });
-
   it("should return `Fizz` for 63", function() {
     expect(lib.fizzBuzz(63)).toEqual('Fizz');
   });
@@ -35,6 +37,18 @@ describe("Fizz Buzz tests ", function() {
 
   it("should return 101 since its indivisible by 3 and 5", function() {
     expect(lib.fizzBuzz(101)).toEqual(101);
+  });
+
+  it("should return typoof string  for 5", function() {
+    expect(typeof lib.fizzBuzz(5)).toEqual('string');
+  });
+
+  it("should return typoof string  for 3", function() {
+    expect(typeof lib.fizzBuzz(3)).toEqual('string');
+  });
+
+  it("should return typoof number  for 3", function() {
+    expect(typeof lib.fizzBuzz(8)).toEqual('number');
   });
 
 })
