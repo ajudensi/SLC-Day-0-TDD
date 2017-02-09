930ca27b4784e6da05784cdaedaa8f1909987212
AJUdensi <a.joshuaudensi@gmail.com>
2017-02-09 10:30:41 +0100

    fizzBuzz function throw error for invalid argument

--- fizzBuzz/library.js
+++ fizzBuzz/library.js
@@ -1,8 +1,9 @@
-'use strict'
+'use strict';
 
 module.exports = {
 
 	fizzBuzz: function (arg) {
+			if(typeof arg !== 'number') throw new Error('Only a numbe is required')
 		  if (arg % 3 === 0 && arg % 5 === 0) {
 		    return "FizzBuzz";
 		  } else if (arg % 3 === 0) {
