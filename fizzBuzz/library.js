dcc197a9bd10347ae3af58cdcf11e5dceb444ed3
AJUdensi <a.joshuaudensi@gmail.com>
2017-02-11 00:12:47 +0100

    indentation fixed

--- fizzBuzz/library.js
+++ fizzBuzz/library.js
@@ -3,7 +3,7 @@
 module.exports = {
 
 	fizzBuzz: function (arg) {
-			if(typeof arg !== 'number') throw new Error('Only a numbe is required')
+			if(typeof arg !== 'number') throw new Error('Only a number is required')
 		  if (arg % 3 === 0 && arg % 5 === 0) {
 		    return "FizzBuzz";
 		  } else if (arg % 3 === 0) {
